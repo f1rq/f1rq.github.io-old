99a4ab899d8c27da1536e9b3b3ca148834574d38
Fabio <forgistm.pl@gmail.com>
2023-02-20 09:59:12 +0100

    .

--- main.js
+++ main.js
@@ -14,3 +14,21 @@ document.querySelectorAll('details').forEach(d =>
 {
   d.addEventListener('toggle', detailsToggled);
 });
+
+
+
+
+
+
+function copy(that){
+  var inp =document.createElement('input');
+  document.body.appendChild(inp)
+  inp.value =that.textContent
+  inp.select();
+  document.execCommand('copy',false);
+  inp.remove();
+  /** WORK IN PROGRESS
+  document.getElementById("alert").innerHTML = "Copied the text.";
+  
+  document.getElementById("alert").innerHTML = "";  */
+  }
